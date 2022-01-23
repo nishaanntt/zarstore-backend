04213be8fd964a51588f1f81d21305678ad98374
Nishant Parashar <nparashar1998@gmail.com>
2022-01-23 16:28:03 +0000

    added update product route

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,17 +1,6 @@
 const Product = require('../models/product.model');
 
-//Create Product
-// exports.createProduct = async (req, res, next) => {
-// 	// const product = await Product.create(req.body);
-
-// 	res.status(201).json({
-// 		message: 'Routes is working',
-// 		// success: true,
-// 		// product,
-// 	});
-// };
-
-//Create Product
+//Create Product -- Admin
 exports.createProduct = async (req, res, next) => {
 	const product = await Product.create(req.body);
 
@@ -22,8 +11,34 @@ exports.createProduct = async (req, res, next) => {
 	});
 };
 
-exports.getAllProducts = (req, res) => {
+//List Products
+exports.getAllProducts = async (req, res) => {
+	const products = await Product.find();
 	res.status(200).json({
-		message: 'Route is working',
+		success: true,
+		products,
+	});
+};
+
+// Update Product -- Admin
+exports.updateProduct = async (req, res) => {
+	let product = await Product.findById(req.params.id);
+
+	if (!product) {
+		return res.status(500).json({
+			success: false,
+			message: 'Product Not Found',
+		});
+	}
+
+	product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+		runValidators: true,
+		useFindAndModify: false,
+	});
+
+	res.status(200).json({
+		success: true,
+		product,
 	});
 };
