6a384c4372730dd2f3f7957256ce0401e32b1a3b
Nishant Parashar <nparashar1998@gmail.com>
2022-01-23 23:02:27 +0000

    added filter and price

--- src/utils/apiFeatures.js
+++ src/utils/apiFeatures.js
@@ -17,6 +17,23 @@ class ApiFeatures {
 		this.query = this.query.find({ ...keyword });
 		return this;
 	}
+
+	filter() {
+		const queryCopy = { ...this.queryStr };
+
+		// Remove fields for category
+		const removeFields = ['keyword', 'page', 'limit'];
+
+		removeFields.forEach(key => delete queryCopy[key]);
+
+		// Filter for Price and Rating
+		let queryStr = JSON.stringify(queryCopy);
+		queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, key => `$${key}`);
+
+		this.query = this.query.find(JSON.parse(queryStr));
+
+		return this;
+	}
 }
 
 module.exports = ApiFeatures;
