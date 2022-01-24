04bbef3fc58675d75151694e3893c308861a8cbd
Nishant Parashar <nparashar1998@gmail.com>
2022-01-24 00:16:56 +0000

    added register and login

--- src/app.js
+++ src/app.js
@@ -6,8 +6,10 @@ app.use(express.json());
 
 //Route Imports
 const product = require('./routes/product.routes');
+const user = require('./routes/user.routes');
 
 app.use('/api/v1', product);
+app.use('/api/v1', user);
 
 // Middleware
 // error
